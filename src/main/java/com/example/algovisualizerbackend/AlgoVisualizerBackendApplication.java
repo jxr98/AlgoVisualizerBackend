de806129b4493ee24b0ab0ccffe6cf9b5735fe6e
Xinrui Jiang <x246jiang@uwaterloo.ca>
2024-07-06 17:53:56 -0400

    deleted useless print logs

--- src/main/java/com/example/algovisualizerbackend/AlgoVisualizerBackendApplication.java
+++ src/main/java/com/example/algovisualizerbackend/AlgoVisualizerBackendApplication.java
@@ -8,7 +8,6 @@ public class AlgoVisualizerBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlgoVisualizerBackendApplication.class, args);
-        System.out.println("sss");
     }
 
 }
